4ba5fabd7941a9e501af1e007d3d72220d163cbf
Clay <claytucker14@gmail.com>
2022-03-23 19:23:42 -0400

    Update

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-window.prompt(weeklyExpenseQuestions[0])
+ // Your Code Here
+
+ window.prompt(weeklyExpenseQuestions[0])
+
+ let answer = window.prompt(weeklyExpenseQuestions[1])
+ console.log(answer)
