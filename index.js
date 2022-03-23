ab77a9a5119a2a79ff79d1ca160c5e6f5289d2cb
Clay <claytucker14@gmail.com>
2022-03-23 19:08:51 -0400

    copied code

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
-window.prompt (weeklyExpenseQuestions[0])
-let answer = window.prompt(weeklyExpenseQuestions[0])
-console.log(answer)
+
+window.prompt(weeklyExpenseQuestions[0])
